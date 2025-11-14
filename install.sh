2d6ccaecbf12c213b87cbb5cced9b06f28229456
q1989233-cell <q1989233@gmail.com>
2025-11-14 14:14:36 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,3 @@
 wget https://download.java.net/openjdk/jdk13/ri/openjdk-13+33_linux-x64_bin.tar.gz
-
 tar -zxvf openjdk-13+33_linux-x64_bin.tar.gz
-
 wget https://fill-data.papermc.io/v1/objects/bd8ec5cdb22370d37816a6de26798df3d2b0d6f9c7c96c88ca45a1303fea50e8/paper-1.14.4-245.jar
